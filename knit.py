4466639c2b196a913dafe3ded8a2b3db5968d0ca
dvasha <dvasha@campus.technion.ac.il>
2025-02-04 21:44:40 +0200

    cleanup

--- knit.py
+++ knit.py
@@ -8,6 +8,12 @@ class gauge:
         self.rows = int(rows)
     def print(self):
         print(self.rows, self.stitches)
+
+def getgauge():
+    string = input('enter your gauge in the following format:\n stitches, rows\n')
+    x, y = string.split(',')
+    return gauge(float(x), float(y)) 
+
 class knittingpattern():
     def __init__(self, txt, gauge):
         self.txtfile = txt
@@ -15,8 +21,13 @@ class knittingpattern():
 
     def print(self):
         print(vars(self.knitgauge))
-  
-def roundUp(floatVal, rem):
+
+def translateGauge(oldGauge : gauge, newGauge : gauge):
+    ratioS = float(1) / oldGauge.stitches * newGauge.stitches
+    ratioR = float(1) / oldGauge.rows * newGauge.rows
+    return ratioS, ratioR
+
+def smartRoundUp(floatVal, rem):
     decimal = rem + floatVal - math.floor(floatVal)
     if (decimal < 0.5):
         resultint = math.floor(floatVal)
@@ -26,10 +37,10 @@ def roundUp(floatVal, rem):
         rem = 1 - decimal 
     return resultint, rem
 
-def translateGauge(oldGauge : gauge, newGauge : gauge):
-    ratioS = float(1) / oldGauge.stitches * newGauge.stitches
-    ratioR = float(1) / oldGauge.rows * newGauge.rows
-    return ratioS, ratioR
+def maxDiff(a,b,c):
+    lst = [abs(a-b), abs(b-c)]
+    return max(lst)
+
 def translate(knittingpattern, newgauge: gauge):
     ratioS, ratioR = translateGauge(knittingpattern.knitgauge, newgauge)
     remS, remR = 0, 0
@@ -41,14 +52,14 @@ def translate(knittingpattern, newgauge: gauge):
                 case ["gauge:", _, _]:
                     f.write(f"gauge: {newgauge.stitches},{newgauge.rows}\n")
                 case ["co", values]:
-                    newS, remS = roundUp(float(values)*ratioS, remS)
+                    newS, remS = smartRoundUp(float(values)*ratioS, remS)
                     f.write(f"co {newS}\n")
                 case ["stk", values]:
-                    newR, remR = roundUp(float(values)*ratioR, remR)
+                    newR, remR = smartRoundUp(float(values)*ratioR, remR)
                     f.write(f"stk {newR}\n")
                 case ["for", height, "inc", width]:
-                    newR, remR = roundUp(float(height)*ratioR, remR)
-                    newS, remS = roundUp(float(width)*ratioS, remS)
+                    newR, remR = smartRoundUp(float(height)*ratioR, remR)
+                    newS, remS = smartRoundUp(float(width)*ratioS, remS)
                     f.write(f"for {newR} inc {newS}:\n")
                     x,y, z = calculateFor(newR, newS)
                     if(x<0):
@@ -61,8 +72,8 @@ def translate(knittingpattern, newgauge: gauge):
                         if(z>0):
                             f.write(f"\t inc 1 every row {z} times \n")
                 case ["for", height, "dec", width]:
-                    newR, remR = roundUp(float(height)*ratioR, remR)
-                    newS, remS = roundUp(float(width)*ratioS, remS)
+                    newR, remR = smartRoundUp(float(height)*ratioR, remR)
+                    newS, remS = smartRoundUp(float(width)*ratioS, remS)
                     f.write(f"for {newR} dec {newS}:\n")
                     x,y, z = calculateFor(newR, newS)
                     if(x<0):
@@ -76,14 +87,13 @@ def translate(knittingpattern, newgauge: gauge):
                             f.write(f"\t dec 1 every row {z} times \n")
                 case ["inc", values]:
                     
-                    newS, remS = roundUp(float(values)*ratioS, remS)
+                    newS, remS = smartRoundUp(float(values)*ratioS, remS)
                     f.write(f"inc {newS}\n")
                 case ["dec", values]:
-                    newS, remS = roundUp(float(values)*ratioS, remS)
+                    newS, remS = smartRoundUp(float(values)*ratioS, remS)
                     f.write(f"dec {newS}\n")
                 case [rest]:
-                    f.write(f"{rest}\n")
-      
+                    f.write(f"{rest}\n")     
 def getknittingpattern(patternname):
     with open(patternname, "r") as f:
         txt = f.readlines()
@@ -93,15 +103,6 @@ def getknittingpattern(patternname):
     knitgauge = gauge(s,r)
     return knittingpattern(txt, knitgauge)
  
-def getgauge():
-    string = input('enter your gauge in the following format:\n stitches, rows\n')
-    x, y = string.split(',')
-    return gauge(float(x), float(y)) 
-
-def maxDiff(a,b,c):
-    lst = [abs(a-b), abs(b-c)]
-    return max(lst)
-
 # W <= H <= 3 W 
 #optimal solution such that 3x+2y+z=Height and x+y+z=Width, with x<=y<=z and difference between x-y, y-z are minimal/ 
 def calculateFor(H, W):
@@ -134,11 +135,6 @@ def calculateFor(H, W):
     # currently no need, since such shallow tapers do not exist in my patterns.
     # I will note that cases where W > H do exist in my knitting, but rarely and I would rather handle them manually.
         
-
-
-    
-
-
 def main():
     # currentgauge = getgauge()
     currentgauge = gauge(22,31)
@@ -146,16 +142,7 @@ def main():
     knittingpattern.print()
     print(translateGauge(knittingpattern.knitgauge, currentgauge))
     translate(knittingpattern, currentgauge)
-    
-    
-# for w in range (1, 200):
-#     for h in range (w, 3*w+1):
-#         res = calculateFor(h, w)
-#         if (res[0] == -1):
-#             print("ERROR for: {h}, {w}.\n")
-
 
 if __name__ == "__main__":
     main()
 
-    #21 or 15 + 6 14 + 3 + 4 15+2 14 + 1 + 2
